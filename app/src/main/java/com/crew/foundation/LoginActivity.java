7e7fc9aa1dc7d4608fef3091b0dca63b318620cc
efraimwijaya <efraimwijaya@outlook.com>
2019-04-17 15:44:50 +0700

    Initial commit

---
+++ app/src/main/java/com/crew/foundation/LoginActivity.java
@@ -0,0 +1,81 @@
+package com.crew.foundation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button loginBtn;
+    TextInputEditText usernameET;
+    TextInputEditText passwordET;
+    DatabaseReference mDatabase;
+    View view;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginBtn = findViewById(R.id.loginbutton);
+        usernameET = findViewById(R.id.login_username);
+        passwordET = findViewById(R.id.login_password);
+        view = findViewById(android.R.id.content);
+        //snackbar buat error login
+        final Snackbar errorSnackbar = Snackbar.make(view,"Username atau Password salah",2000);
+
+        //jika login button ditekan
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                //arahin firebase ke table users
+                mDatabase = FirebaseDatabase.getInstance().getReference().child("users");
+                mDatabase.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        //ambil isi dari edtText username dan password
+                        String username = usernameET.getText().toString();
+                        String password = passwordET.getText().toString();
+                        //looping tiap baris users
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren() ){
+                            String username_check = snapshot.child("username").getValue(String.class);
+                            String password_check = snapshot.child("password").getValue(String.class);
+                            if (username.equals(username_check) && password.equals(password_check) ){
+                                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                                startActivity(intent);
+                                //butuh return agar snackbar tidak muncul jika berhasil login
+                                return;
+                            }
+                        }
+                        //tampilkan error message jika salah username/password
+                        errorSnackbar.show();
+
+
+
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    }
+                });
+
+
+
+            }
+        });
+
+    }
+}
